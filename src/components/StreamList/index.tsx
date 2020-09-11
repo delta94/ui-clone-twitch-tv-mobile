d206d47eb68df99aba5940ffe9ac572816cd88aa
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-09-11 20:49:12 -0300

    Add project code

---
+++ src/components/StreamList/index.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+import streamThumbnail from '../../images/stream_thumbnail.jpg';
+
+import {
+  List,
+  StreamContainer,
+  StreamThumbnail,
+  StreamColumn,
+  StreamRow,
+  StreamHeader,
+  StreamDescription,
+  StreamCategory,
+  StreamAvatar,
+  StreamUsername,
+  TagRow,
+  TagView,
+  TagText,
+} from './styles';
+
+const StreamList: React.FC = () => {
+  const StreamItem = () => (
+    <StreamContainer>
+      <StreamThumbnail source={streamThumbnail} />
+
+      <StreamColumn>
+        <StreamRow>
+          <StreamHeader>
+            <StreamAvatar />
+            <StreamUsername numberOfLines={1}>gideon_fernandes</StreamUsername>
+          </StreamHeader>
+
+          <StreamDescription numberOfLines={1}>
+            Front-end with Next.js, Chakra UI and GraphQL
+          </StreamDescription>
+
+          <StreamCategory numberOfLines={1}>
+            Science and Technology
+          </StreamCategory>
+        </StreamRow>
+
+        <TagRow>
+          <TagView>
+            <TagText>Portuguese</TagText>
+          </TagView>
+          <TagView>
+            <TagText>Web Development</TagText>
+          </TagView>
+        </TagRow>
+      </StreamColumn>
+    </StreamContainer>
+  );
+
+  return (
+    <List>
+      <StreamItem />
+      <StreamItem />
+      <StreamItem />
+      <StreamItem />
+    </List>
+  );
+};
+
+export default StreamList;
